03a1939e8280ded3a511ba6a988b25a75724a100
Jakub Michalski <jakubmichalski01@gmail.com>
2023-04-14 22:12:57 +0200

    Add CountryProvider service

---
+++ src/services/CountryProvider.ts
@@ -0,0 +1,58 @@
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import Country from "../models/Country";
+import getMultipleRandomElements from "../utility/getMultipleRandomElements";
+
+export default class CountryProvider {
+    private graphqlClient = new ApolloClient({
+        uri: 'https://countries.trevorblades.com/graphql',
+        cache: new InMemoryCache(),
+    });
+    private getCountriesQuery = gql`query GetCountries($continent: String) {
+        countries(filter: { continent: { eq: $continent } }) {
+            name
+        }
+    }`;
+    
+    private async getCountry(name: string): Promise<Country | string> {
+        const response = await fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,currencies,subregion,languages`);
+
+        if (!response.ok) {
+            return name;
+        }
+
+        const matching = await response.json() as any[];
+
+        if (matching.length === 0) {
+            return name;
+        }
+
+        const first = matching[0];
+
+        return {
+            name: first.name,
+            capital: first.capital,
+            population: first.population,
+            subregion: first.subregion,
+            languages: Object.values(first.languages),
+            currencies: Object.values(first.currencies)
+        };
+    }
+
+    public async getCountries(continent: string, count: number): Promise<{ countries?: Array<Country | string> }> {
+        const { data } = await this.graphqlClient.query({ query: this.getCountriesQuery, variables: { continent } });
+
+        if (data === undefined) {
+            return {};
+        }
+
+        const names = data.countries.map((country: any) => country.name);
+
+        const randomNames = getMultipleRandomElements(names, count).sort();
+
+        const tasks = randomNames.map(name => this.getCountry(name));
+
+        const countries = await Promise.all(tasks);
+
+        return { countries };
+    }
+}
